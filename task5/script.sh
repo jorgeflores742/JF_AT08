4b43dd41a659d5792682a592fc7baa31c17c1520
Jorge Flores <31511750+jorgeflores742@users.noreply.github.com>
2019-02-21 11:15:07 -0400

    Update script.sh

--- task5/script.sh
+++ task5/script.sh
@@ -1,18 +1,13 @@
 #!/usr/bin/env bash
 
-echo "~ Installing Docker ~"
-#install Docker
-sudo snap install docker
-
-echo "~ Installing Docker-compose ~"
-#install Docker-compose
-sudo apt-get install docker-compose -y
-
 echo "~ Installing java 8 ~"
 #install java
 sudo apt update
 sudo apt-get install openjdk-8-jdk -y
 
 echo "~ Installing jenkins ~"
-#install jenkins:2.107.3-alpine 
-docker pull jenkins/jenkins:lts
+#install jenkins:2.105.3 
+wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install jenkins=2.150.3 -y
